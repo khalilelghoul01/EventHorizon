f10a76916c4a27351af5444a738fbef82a363e86
Khalil El Ghoul <khalilelghoul01@gmail.com>
2022-07-07 15:32:22 +0200

    update

--- extensions/js.py
+++ extensions/js.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
-
+import re
 from utils.selector import SelectorType
 
 
 def handleStep(driver: webdriver, step: dict, memory: dict):
     path = step['path'] if 'path' in step else None
     code = step['[text]'] if '[text]' in step else None
+
     if path is None and code is None:
         raise Exception('No path or code specified')
     if path is not None:
